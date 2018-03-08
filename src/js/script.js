3c2da39efd03bd1e6dbcdf5fa1a2687813556436
zhurgan <gosha_zhuravlev_2015@mail.ru>
2018-03-08 19:33:46 +0300

    components stucrure modified, slider controls added

--- src/js/script.js
+++ src/js/script.js
@@ -5,24 +5,39 @@ import {videoList} from './json-data.js';
 window.onload = function(){
 
     Vue.component('video-comp', {
-        template: '<video v-on:ended="incrementVideoNum" autoplay muted v-bind:poster="videos[videoNum].poster" controls\n' +
-        'v-bind:src="videos[videoNum].src" width="100%"\n' +
+        template: '<video v-on:ended="showNextVideo" autoplay muted v-bind:poster="videos[vNum].poster" controls\n' +
+        'v-bind:src="videos[vNum].src" width="100%"\n' +
         '></video>',
-        data: function () {
-            return {
-                videos: videoList,
-                videoNum: 0
-            }
-        },
+        props: ['vNum', 'videos'],
         methods: {
-            incrementVideoNum: function () {
-                this.videoNum++;
+            showNextVideo: function(){
+                this.$emit('increment');
             }
         }
     });
 
     var videoWrapper = new Vue({
-        el: '.video-container'
+        el: '.video-container',
+        data: {
+            items: videoList,
+            videoNum: 0
+        },
+        methods: {
+            incrementVideoNum: function () {
+                if (this.items[this.videoNum+1]){
+                    this.videoNum++;
+                } else {
+                    this.videoNum = 0;
+                }
+            },
+            decrementVideoNum: function () {
+                if (this.items[this.videoNum-1]){
+                    this.videoNum--;
+                } else {
+                    this.videoNum = this.items.length - 1;
+                }
+            }
+    }
     });
 };
 
